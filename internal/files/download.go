15f638a73b68c2280bdef7585c04cf8d3b63d877
Linghua Zhang <linghua.zhang@me.com>
2025-01-26 19:19:12 +0900

    test: unittest

--- internal/files/download.go
+++ internal/files/download.go
@@ -10,7 +10,22 @@ import (
 	"github.com/waynezhang/toyskkserv/internal/tcp"
 )
 
+type downloadFn func(url string, dst string) error
+type serverNotifyFn func()
+
+func httpDownload(url string, dst string) error {
+	return getter.GetFile(dst, url)
+}
+
+func tcpServerNotifyFn() {
+	tcp.SendReloadCommand(config.Shared().ListenAddr)
+}
+
 func UpdateDictionaries(urls []string, dictDirectory string) {
+	updateDictionaries(urls, dictDirectory, httpDownload, tcpServerNotifyFn)
+}
+
+func updateDictionaries(urls []string, dictDirectory string, dnFn downloadFn, notifyFn serverNotifyFn) {
 	slog.Info("Updating dictionaries", "urls", urls, "dictDirectory", dictDirectory)
 
 	err := os.MkdirAll(dictDirectory, os.ModePerm)
@@ -20,28 +35,38 @@ func UpdateDictionaries(urls []string, dictDirectory string) {
 
 	updated := false
 	for _, url := range urls {
+		if IsLocalURL(url) {
+			slog.Info("Skip local file", "url", url)
+			continue
+		}
+
 		path := filepath.Join(dictDirectory, filepath.Base(url))
-		u, err := Download(url, path)
+		u, err := download(url, path, dnFn)
 		if err != nil {
 			slog.Error("Failed to download file", "url", url)
 			continue
 		}
+
 		updated = updated || u
 	}
 
 	if updated {
-		tcp.SendReloadCommand(config.Shared().ListenAddr)
+		notifyFn()
 	}
 }
 
-func Download(url, tofile string) (updated bool, err error) {
+func Download(url string, tofile string) (updated bool, err error) {
+	return download(url, tofile, httpDownload)
+}
+
+func download(url string, tofile string, dnFn downloadFn) (updated bool, err error) {
 	oldChecksum, err := FileChecksum(tofile)
 	if err != nil && IsFileExisting(tofile) {
 		slog.Error("Failed to get checksum from existing file", "path", tofile, "err", err)
 	}
 
 	slog.Info("Downloading file", "file", tofile)
-	if err := getter.GetFile(tofile, url); err != nil {
+	if err := dnFn(url, tofile); err != nil {
 		os.Remove(tofile)
 		return false, err
 	}
@@ -49,6 +74,8 @@ func Download(url, tofile string) (updated bool, err error) {
 	newChecksum, err := FileChecksum(tofile)
 	if err != nil {
 		slog.Error("Failed to get checksum from new downloaded file", "path", tofile, "err", err)
+		os.Remove(tofile)
+		return false, err
 	}
 
 	if oldChecksum == newChecksum {
--- internal/files/download_test.go
+++ internal/files/download_test.go
@@ -1,55 +1,154 @@
 package files
 
 import (
+	"errors"
 	"os"
 	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
+
+type MockFunction struct {
+	mock.Mock
+	dontCreateFile bool
+}
+
+func (m *MockFunction) download(url string, dst string) error {
+	if !m.dontCreateFile {
+		os.WriteFile(dst, []byte("test data"), 0644)
+	}
+	args := m.Called(url, dst)
+	if args.Get(0) == nil {
+		return nil
+	} else {
+		return args.Error(0)
+	}
+}
+
+func (m *MockFunction) notify() {
+	m.Called()
+}
 
 func TestDownload(t *testing.T) {
 	tmp := prepareTempDir(t)
 	defer os.RemoveAll(tmp)
 
-	file := filepath.Join(tmp, "file1")
-	u, err := Download("https://github.com/skk-dev/dict/raw/refs/heads/master/SKK-JISYO.china_taiwan", file)
-	assert.Nil(t, err)
-	assert.True(t, u)
-	assert.True(t, IsFileExisting(file))
+	url := "https://github.com/skk-dev/dict/raw/refs/heads/master/SKK-JISYO.edict"
+	path := filepath.Join(tmp, "dest_file")
 
-	file = filepath.Join(tmp, "file1")
-	u, err = Download("https://github.com/skk-dev/dict/raw/refs/heads/master/SKK-JISYO.china_taiwan", file)
-	assert.Nil(t, err)
-	assert.False(t, u)
-	assert.True(t, IsFileExisting(file))
+	mockObj := new(MockFunction)
 
-	err = os.WriteFile(file, []byte("some other data to change file"), 0644)
+	// new downloaded case
+	mockObj.On("download", url, path).Return(nil)
+	updated, err := download(url, path, mockObj.download)
+	assert.True(t, updated)
 	assert.Nil(t, err)
 
-	u, err = Download("https://github.com/skk-dev/dict/raw/refs/heads/master/SKK-JISYO.china_taiwan", file)
-	assert.Nil(t, err)
-	assert.True(t, u)
-	assert.True(t, IsFileExisting(file))
+	// updated case
+	assert.Nil(t, os.RemoveAll(path))
+	assert.Nil(t, os.WriteFile(path, []byte("original data"), 0644))
 
-	file = filepath.Join(tmp, "file2")
-	u, err = Download("https://github.com/skk-dev/dict/raw/refs/heads/master/SKK-JISYO.china_taiwan_notexisting", file)
+	mockObj.On("download", url, path).Return(nil)
+	updated, err = download(url, path, mockObj.download)
+	assert.True(t, updated)
+	assert.Nil(t, err)
+
+	// not updated case
+	assert.Nil(t, os.RemoveAll(path))
+	assert.Nil(t, os.WriteFile(path, []byte("test data"), 0644))
+
+	mockObj.On("download", url, path).Return(nil)
+	updated, err = download(url, path, mockObj.download)
+	assert.False(t, updated)
+	assert.Nil(t, err)
+
+	// download succeed but no file created
+	assert.Nil(t, os.RemoveAll(path))
+
+	mockObj.dontCreateFile = true
+	mockObj.On("download", url, path).Return(errors.New("some error"))
+	updated, err = download(url, path, mockObj.download)
+	assert.False(t, updated)
 	assert.NotNil(t, err)
-	assert.False(t, u)
-	assert.False(t, IsFileExisting(file))
+
+	// download failed
+	assert.Nil(t, os.RemoveAll(path))
+	assert.Nil(t, os.WriteFile(path, []byte("test data"), 0644))
+
+	mockObj.On("download", mock.Anything, mock.Anything).Unset()
+	mockObj.On("download", url, path).Return(errors.New("some error"))
+	updated, err = download(url, path, mockObj.download)
+	assert.False(t, updated)
+	assert.NotNil(t, err)
 }
 
 func TestUpdateDictionary(t *testing.T) {
 	tmp := prepareTempDir(t)
 	defer os.RemoveAll(tmp)
 
-	UpdateDictionaries([]string{
+	mockObj := new(MockFunction)
+	mockObj.On("notify").Return()
+
+	urls := []string{
 		"https://github.com/skk-dev/dict/raw/refs/heads/master/SKK-JISYO.edict",
 		"https://github.com/skk-dev/dict/raw/refs/heads/master/SKK-JISYO.china_taiwan",
-	}, tmp)
+		filepath.Join(tmp, "some_local_file"),
+	}
+	paths := []string{
+		filepath.Join(tmp, "SKK-JISYO.edict"),
+		filepath.Join(tmp, "SKK-JISYO.china_taiwan"),
+		filepath.Join(tmp, "some_local_file"),
+	}
+	assert.Nil(t, os.WriteFile(paths[0], []byte("original data"), 0644))
+	assert.Nil(t, os.WriteFile(paths[1], []byte("original data"), 0644))
+	assert.Nil(t, os.WriteFile(paths[2], []byte("original data"), 0644))
+
+	mockObj.On("download", urls[0], paths[0]).Return(nil)
+	mockObj.On("download", urls[1], paths[1]).Return(nil)
+	mockObj.On("download", urls[2], paths[2]).Panic("not supposed to be called")
+
+	updateDictionaries(urls, tmp, mockObj.download, mockObj.notify)
+
+	mockObj.AssertCalled(t, "download", urls[0], paths[0])
+	mockObj.AssertCalled(t, "download", urls[1], paths[1])
+	mockObj.AssertCalled(t, "notify")
+
+	// no update case
+
+	mockObj2 := new(MockFunction)
+	mockObj2.On("download", mock.Anything, mock.Anything).Return(nil)
+	mockObj2.On("notify").Return(nil)
+	assert.Nil(t, os.WriteFile(paths[0], []byte("test data"), 0644))
+	assert.Nil(t, os.WriteFile(paths[1], []byte("test data"), 0644))
+	assert.Nil(t, os.WriteFile(paths[2], []byte("test data"), 0644))
+
+	updateDictionaries(urls, tmp, mockObj2.download, mockObj2.notify)
+
+	mockObj2.AssertCalled(t, "download", urls[0], paths[0])
+	mockObj2.AssertCalled(t, "download", urls[1], paths[1])
+	mockObj2.AssertNotCalled(t, "notify")
+}
+
+func TestRealDownload(t *testing.T) {
+	tmp := prepareTempDir(t)
+	defer os.RemoveAll(tmp)
+
+	urls := []string{
+		"https://github.com/skk-dev/dict/raw/refs/heads/master/SKK-JISYO.edict",
+		"https://github.com/skk-dev/dict/raw/refs/heads/master/SKK-JISYO.china_taiwan",
+		filepath.Join(tmp, "some_local_file"),
+	}
+
+	mockObj := new(MockFunction)
+	mockObj.On("notify").Return()
+
+	updateDictionaries(urls, tmp, httpDownload, mockObj.notify)
 
 	assert.True(t, IsFileExisting(filepath.Join(tmp, "SKK-JISYO.edict")))
 	assert.True(t, IsFileExisting(filepath.Join(tmp, "SKK-JISYO.china_taiwan")))
+	mockObj.AssertCalled(t, "notify")
 }
 
 // helper func
